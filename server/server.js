20b58bea9ef67b1d1412d2027686d24dca564d6f
seguteja <sivateja@skystop.tech>
2021-11-16 15:27:55 +0530

    latest

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 const path=require('path');
+const http=require('http')
 const express=require('express');
+const socketIO=require('socket.io');
+
 
 const publicPath=path.join(__dirname,'/../public');
-const app=express();
 const port=process.env.PORT || 9999;
+const app=express();
+
+let server=http.createServer(app);
+let io=socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection',(socket)=>{
+console.log("a new user just connected")
+
+socket.on('disconnect',(socket)=>{
+    console.log("a new user  disconnected")
+    });
+});
 
 
-app.use(express.static(publicPath))
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`server is started on ${port}`);
 });
